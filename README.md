d00e1362ab7f29645633c7afa2deefe76b084ac2
TheSeaWave <polakovaleksandr026@gmail.com>
2024-10-13 21:20:17 +0300

    123

---
+++ README.md
@@ -0,0 +1,2 @@
+запуск бенчмарков 
+go test -bench=. -benchtime=1s -benchmem > /home/student/dz1/benchmark_results/benchmark_results.txt
--- cmd/internal/pkg/storage/storage_test.go
+++ cmd/internal/pkg/storage/storage_test.go
@@ -1,6 +1,7 @@
 package storage
 
 import (
+	"strconv"
 	"testing"
 )
 
@@ -41,5 +42,48 @@ func TestSetGetWithKind(t *testing.T) {
 				t.Errorf("expected value kind: %v", c.kind)
 			}
 		})
+	}
+}
+
+func BenchmarkStorage_Set(b *testing.B) {
+	storage, err := NewStorage()
+	if err != nil {
+		b.Fatalf("Error creating storage: %v", err)
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		storage.Set("key"+strconv.Itoa(i), "value"+strconv.Itoa(i))
+	}
+}
+
+func BenchmarkStorage_Get(b *testing.B) {
+	storage, err := NewStorage()
+	if err != nil {
+		b.Fatalf("Error creating storage: %v", err)
+	}
+
+	for i := 0; i < 1000; i++ {
+		storage.Set("key"+strconv.Itoa(i), "value"+strconv.Itoa(i))
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		storage.Get("key" + strconv.Itoa(i%1000))
+	}
+}
+
+func BenchmarkStorage_SetThenGet(b *testing.B) {
+
+	storage, err := NewStorage()
+	if err != nil {
+		b.Fatalf("Error creating storage: %v", err)
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		key := "key" + strconv.Itoa(i)
+		storage.Set(key, "value"+strconv.Itoa(i))
+		storage.Get(key)
 	}
 }
